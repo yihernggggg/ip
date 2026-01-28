678c8be2e41b907b8e5439095156177c3b911d44
YHWINDOWS\chang <changyiherng2004@gmail.com>
2026-01-28 15:29:46 +0800

    Implemented simple greeting and exit message.

---
+++ src/main/java/Flight.java
@@ -0,0 +1,20 @@
+public class Flight {
+    public static void main(String[] args) {
+        String logo = "___________.__  .__       .__     __   \n" +
+                "\\_   _____/|  | |__| ____ |  |___/  |_ \n" +
+                " |    __)  |  | |  |/ ___\\|  |  \\   __\\\n" +
+                " |     \\   |  |_|  / /_/  >   Y  \\  |  \n" +
+                " \\___  /   |____/__\\___  /|___|  /__|  \n" +
+                "     \\/           /_____/      \\/      ";
+
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Flight\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println(greeting);
+    }
+}
